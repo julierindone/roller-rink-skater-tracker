ed956fd6e270ddec0d36d4aff615b8a579a91110
Julie Rindone <julie.rindone@gmail.com>
2023-08-20 16:41:51 -0500

    Added am/pm conditionals

--- Passenger-Counter/index.js
+++ Passenger-Counter/index.js
@@ -1,20 +1,52 @@
+// TODO: add daily skater total at bottom
+
 let saveEl = document.getElementById("save-el")
 let count = 0
 let countEl = document.getElementById("count-el")
-let hour = 6
-let hourEl = document.getElementById("hour-el")
+let startTime = 6
+let startTimeEl = document.getElementById("startTime-el")
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1
+  countEl.textContent = count
 }
 
 function save() {
-    // let countStr = count + " - "
-    let endTime = hour + 1
-    let startEnd = `${hour} - ${endTime}pm: ` + count + "\n"
+
+  let endTime = (startTime + 1)
+  console.log(`line 17 -  endtime is ${endTime}`)
+
+  if (endTime < 6) {
+    let startEnd = `${startTime} - ${endTime}am ` + count + "\n"
+
     saveEl.innerText += startEnd
     countEl.textContent = 0
     count = 0
-    hour += 1
+    startTime += 1
+    startEnd = `${startTime} - ${endTime}am ` + count + "\n"
+
+  }
+
+  else if (endTime == 13) {
+    endTime = 1
+    let startEnd = `${startTime} - ${endTime}am ` + count + "\n"
+
+    saveEl.innerText += startEnd
+    countEl.textContent = 0
+    count = 0
+    startTime = 1
+    startEnd = `${startTime} - ${endTime}am ` + count + "\n"
+  }
+
+  else {
+    let startEnd = `${startTime} - ${endTime}pm: ${count}` + "\n"
+
+    saveEl.innerText += startEnd
+    countEl.textContent = 0
+    count = 0
+    startTime += 1
+  }
+  console.log(`The endtime after the save function is ${endTime}.`)
 }
+
+    // else endTime = endTime + startTime
